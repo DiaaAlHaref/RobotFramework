ab70afeae450548bf684236d4a094b2932e572a7
Diaa AlHaref <diaa.alharef@gmail.com>
2020-10-01 15:44:00 +0200

    Adding login Page Element Locators

--- PageObjects/ElementLocators.py
+++ PageObjects/ElementLocators.py
@@ -6,4 +6,12 @@ phone_Locator="id:inputPhone"
 companyName_Locator="id:inputCompanyName"
 streetAddress_Locator="id:inputAddress1"
 city_Locator="id:inputCity"
+passwordInput1_Locator="id:inputNewPassword1"
+confirmPassword_Locator="id:inputNewPassword2"
 registerButton_Locator="xpath://*[@value='Register']"
+
+
+#LoginPage Element Locator
+emailAddress_Locator="id:inputEmail"
+password_Locator="id:inputPassword"
+loginButton_Locator="id:login"
